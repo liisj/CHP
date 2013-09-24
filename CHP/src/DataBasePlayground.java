05f5ce67c6eba07ae36e8dedbcc6d385facedb8d
Peter Büchler <p.buechler@hotmail.com>
2013-09-24 02:04:56 +0200

    exemplary functionality

--- CHP/src/DataBasePlayground.java
+++ CHP/src/DataBasePlayground.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Calendar;
 import java.util.Properties;
 
 public class DataBasePlayground {
@@ -42,10 +43,10 @@ public class DataBasePlayground {
 	public static void init(Connection con) {
 		try {
 			statementPutOrder = con.prepareStatement("INSERT INTO orders "
-					+ "(facility_id,drug_id,unit_number)"
-					+ " VALUES (?,?,?)");
+					+ "(facility_id,drug_id,unit_number,date)"
+					+ " VALUES (?,?,?,?)");
 			statementGetOrders = con.prepareStatement("SELECT o.id, d.med_name,"
-					+ " o.unit_number, d.unit, d.id"
+					+ " o.unit_number, d.unit, d.id, o.date"
 					+ " FROM orders o, drugs d"
 					+ " WHERE o.drug_id = d.id"
 					+ " AND o.facility_id = ?"
@@ -70,6 +71,7 @@ public class DataBasePlayground {
 				statementPutOrder.setInt(1, order[0]);
 				statementPutOrder.setInt(2, order[1]);
 				statementPutOrder.setInt(3, order[2]);
+				statementPutOrder.setDate(4, new java.sql.Date(Calendar.getInstance().getTime().getTime()));
 				statementPutOrder.addBatch();
 			}
 			statementPutOrder.executeBatch();
@@ -77,14 +79,13 @@ public class DataBasePlayground {
 			e.printStackTrace();
 		}
 	}
-
 	
 	/**
 	 * 
 	 * @param con
 	 * @param facility_id
 	 */
-	public static void getOrders(Connection con, int facility_id) {
+	public static void getOrders(int facility_id) {
 		if (statementPutOrder == null) {
 			System.err.println("Init the statements, dude.. (init())");
 			return;
@@ -94,13 +95,15 @@ public class DataBasePlayground {
 			ResultSet rs = statementGetOrders.executeQuery();
 			String med_name, unit;
 			int unit_number, drug_id, order_id;
+			Date order_date;
 			while (rs.next()) {
 				order_id = rs.getInt(1);
 				med_name = rs.getString(2);
 				unit_number = rs.getInt(3);
 				unit = rs.getString(4);
 				drug_id = rs.getInt(5);
-				System.out.println("Order " + order_id + ":   " + drug_id + ": " + med_name + " | " + unit_number
+				order_date = rs.getDate(6);
+				System.out.println(order_date + " - Order " + order_id + ":   " + drug_id + ": " + med_name + " | " + unit_number
 						+ "x " + unit);
 			}
 		} catch (SQLException e) {
@@ -108,10 +111,14 @@ public class DataBasePlayground {
 		}
 	}
 
+	public static void deleteOrder(int order_id) {
+		
+	}
+	
 	public static void main(String[] args) {
 		Connection con = DataBasePlayground.getConnection();
 		DataBasePlayground.init(con);
-		DataBasePlayground.getOrders(con, 1);
+		DataBasePlayground.getOrders(1);
 		int[][] orders = {{1,2,1},{1,3,1},{1,1,1}};
 //		DataBasePlayground.putOrders(orders); //should fail
 //		DataBasePlayground.initPutOrders(con);
