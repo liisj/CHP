1aee3d2ba70b1c7d9a4c7ee136e13d16a680da28
Peter Büchler <p.buechler@hotmail.com>
2013-10-30 16:27:08 +0100

    Fix

--- PortletTest-portlet/docroot/WEB-INF/src/com/test/DataBaseFunctions.java
+++ PortletTest-portlet/docroot/WEB-INF/src/com/test/DataBaseFunctions.java
@@ -286,7 +286,7 @@ public class DataBaseFunctions {
 		String where = "";
 		if (drug_idS != null) {
 			where += p == 0 ? " WHERE " : " AND ";
-			where += "drug_id = ?";
+			where += "id = ?";
 			p++;
 		}
 
